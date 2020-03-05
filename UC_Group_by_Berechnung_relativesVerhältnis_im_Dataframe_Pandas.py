5119e204c210f48c978acbd705cca84142f9f6b9
PeziBear <59111066+ThePeziBear@users.noreply.github.com>
2020-03-05 17:54:13 +0100

    add

--- UC_Group_by_Berechnung_relativesVerhältnis_im_Dataframe_Pandas.py
+++ UC_Group_by_Berechnung_relativesVerhältnis_im_Dataframe_Pandas.py
@@ -2,19 +2,11 @@ import pandas as pd
 import numpy as np
 
 df= pd.read_csv('Occupation_occupation.table', sep='|', index_col = 'user_id')
-#occupation_data= occupation_data.set_index('user_id')
 df.head()
-
 
 df['gender_m'] = df['gender'] == 'M'
 df['gender_m']=df['gender_m'].astype(int)
-#df['gender'] = df['gender'].astype(int)
-
-r= df.groupby('occupation').gender_m.sum()/df.occupation.value_counts()
-r.sort_values(ascending = False)
-
-#occ= df.groupby('occupation')(['gender_m']==1).count()
 
 occ= df.groupby('occupation')['gender'].count()
 occ_m= df.groupby('occupation')['gender_m'].sum()/occ
-occ_m.sort_values(ascending = False)
+result = occ_m.sort_values(ascending = False)
