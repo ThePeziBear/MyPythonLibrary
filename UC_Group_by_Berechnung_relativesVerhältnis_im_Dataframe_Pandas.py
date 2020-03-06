9e352ef3c253d200b3c7bf5a322b3d2f2ff086e3
PeziBear <59111066+ThePeziBear@users.noreply.github.com>
2020-03-06 11:40:39 +0100

    head(), astype(), groupby() with 2 parameters, sum(), sort_values, lambda()

--- UC_Group_by_Berechnung_relativesVerhältnis_im_Dataframe_Pandas.py
+++ UC_Group_by_Berechnung_relativesVerhältnis_im_Dataframe_Pandas.py
@@ -11,3 +11,11 @@ df['gender_m']=df['gender_m'].astype(int)
 occ= df.groupby('occupation')['gender'].count()
 occ_m= df.groupby('occupation')['gender_m'].sum()/occ
 result = occ_m.sort_values(ascending = False)
+
+
+# Variante mit Lambda
+users = df
+
+users['gender_n']=users['gender'].apply(lambda x: 1 if x== 'M' else 0) # gleicher Output wie zeile 8 & 9
+r= users.groupby('occupation').gender_n.sum()/users.occupation.value_counts()
+print(r.sort_values(ascending = False))
