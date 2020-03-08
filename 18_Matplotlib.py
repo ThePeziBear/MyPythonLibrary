9406be721c0c82b7c5875926cb1bd96a2a13607b
PeziBear <59111066+ThePeziBear@users.noreply.github.com>
2020-03-08 17:50:26 +0100

    Matplotlib - Basics

---
+++ 18_Matplotlib.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+plt.plot([1,2,3],[4,5,4], color = '#21c4ed', linestyle='dashed', marker='o')
+# erste Liste die X-Werte, zweite Liste Y-Werte
+# color via HEX - Farbe finden über color picker (google)
+# allgemeine Infos = https://matplotlib.org/api/pyplot_api.html#matplotlib.pyplot.plot
+#linestyle = https://matplotlib.org/api/lines_api.html#matplotlib.lines.Line2D.set_linestyle
+# marker = https://matplotlib.org/api/markers_api.html#module-matplotlib.markers
+plt.show() # Starten der Anzeige
+
