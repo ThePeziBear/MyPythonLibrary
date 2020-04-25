3d859859d33123fd98c66519f328989139468327
ThePeziBear <jernej.peter@gmail.com>
2020-04-25 12:55:15 +0200

    Invest Wohnung

--- For Beginners/Finanzierung_Berechnung.py
+++ For Beginners/Finanzierung_Berechnung.py
@@ -6,17 +6,14 @@ EK_Quote= 0.3
 FK_Quote= 1 - EK_Quote
 
 i = 0.015
-T= 20
-
+T=20
 EK = invest * EK_Quote
 FK = invest * FK_Quote
-
-
-
 
 list=[]
 for e in range (1,20):
     tilgung = invest - (invest/20)
-    list.append(tilgung)
+    tilgung2= tilgung- (invest/20)
+    list.append(tilgung2)
 
 print(list)
