c9844aa5f7a53f9e811091aa40cf662a62c8f0fc
ThePeziBear <jernej.peter@gmail.com>
2020-04-28 21:27:09 +0200

    Invest Wohnung

--- For Beginners/Finanzierung_Berechnung.py
+++ For Beginners/Finanzierung_Berechnung.py
@@ -7,15 +7,39 @@ FK_Quote= 1 - EK_Quote
 
 i = 0.015
 T=20
+T_rate = invest/T
 EK = invest * EK_Quote
 FK = invest * FK_Quote
 
-list1=[]
-list2=[]
-for z in range (1,20):
-    tilgung = int(invest) - int((invest/20))
-    list1.append(tilgung)
+list1 = []
+
+
+def calc():
+    n = 20
+    value = 182000
+    rate = 0.015
+    for i in range(20):
+        result2 = value - (value / n)
+        list1.append(result2)
+        return list1
+
+
+calc()
+
+# result = value * (1+rate) ** 20
+
+# value = float(input("Enter initial value: "))
+# rate = float(input("Enter the annual interest rate: "))
+# time = float(input("Enter the time: "))
+# result = invest_calc(value, rate)
+
+
+#list1=[]
+#list2=[]
+#for z in range (1,20):
+#    tilgung = int(invest) - int((invest/20))
+#    list1.append(tilgung)
 
 
 
-print(list1)
+#print(list1)
