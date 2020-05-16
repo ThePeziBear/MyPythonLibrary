3884d311aab0a1637c62582db9a48355fea58791
ThePeziBear <jernej.peter@gmail.com>
2020-05-16 14:59:50 +0200

    Galton_board

--- For Beginners/Galton Board.py
+++ For Beginners/Galton Board.py
@@ -1,29 +1,23 @@
-from random import randint as ri
+import matplotlib.pyplot as plt
+import random
 
-balls = 100  # number of balls
-slots = 10  # number of base slots
-# you can edit the values for different size of board, and different number of balls
+TOTAL_BALLS = 500
+TOTAL_LINES = 10
 
-res = []
+for i in range(0, TOTAL_BALLS):
+    total = []
+    for i in range(0, TOTAL_LINES):
 
-for i in range(slots):
-    res.append(0)
+        x = random.uniform(0, 1)
+        if x >= 0.5:
+            round(x)
+        else:
+            round(x)
 
-for i in range(balls):
-    subres = [1]
-    for j in range(slots - 1):
-        subres.insert((j + 1) * ri(0, 1), 0)
-    res[subres.index(1)] += 1
-# subres is a subresult of one ball. '1' is the position of the ball. in each galton row it goes left or right - practically there is randomly an additional '0' at the beginning or at the end. 'res' collects each ball's 'subres'
+        total.append(round(x))
 
-for i in range(slots):
-    print((1 + slots - i) * ' ' + i * '. ')
-# hey what a picturesque Galton board!
+        distribution = sum(total)
+        print(distribution)
 
-for i in range(max(res) + 1, 0, -1):
-    [print('|o', end='') if j >= i else print('| ', end='') for j in res]
-    print('|')
-print('-' * (2 * slots + 1))
-# and here are the balls arrived to the slots. the only reason you missed the cool animation the mass of balls bouncing downwards is that python is so fast and you don't! ;)
 
-print(res)
+
