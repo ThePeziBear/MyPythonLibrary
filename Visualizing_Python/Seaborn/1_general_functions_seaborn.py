6afec9418cf59aeaed94d7689d406b0de8c16a3a
ThePeziBear <jernej.peter@gmail.com>
2020-08-19 20:21:43 +0200

    update

--- Visualizing_Python/Seaborn/1_general_functions_seaborn.py
+++ Visualizing_Python/Seaborn/1_general_functions_seaborn.py
@@ -12,3 +12,13 @@ sns.distplot(tips['total_bill'],kde=False #Anzeige der Dichtefunktion True
 sns.jointplot(x='total_bill',y='tip',data=tips #Data = Auswahl Daten
               , kind='scatter')# kind = Dartellungsart
 
+sns.jointplot(x='total_bill',y='tip',data=tips #Data = Auswahl Daten
+              , kind='hex')# Hex ist tolle Darstellung für häufigsten Wert bei der Abhängigkeit von 2 Variablen.
+
+
+sns.jointplot(x='total_bill',y='tip',data=tips #Data = Auswahl Daten
+              , kind='kde')# Kernel Destiniy Darstellung tolle Darstellung für häufigsten Wert bei der Abhängigkeit von 2 Variablen
+
+sns.pairplot(tips, #Das Diagramm pairplot zeigt paarweise Beziehungen in einem kompletten Dataframe. Für kategorische Variablen können wir über das hue Argument die Farbe einstellen.
+             hue='sex',
+             palette='coolwarm')
