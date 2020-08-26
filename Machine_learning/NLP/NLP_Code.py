31b7d3095f951e383b4f6548f23a5d5932c32c47
ThePeziBear <jernej.peter@gmail.com>
2020-08-26 22:03:11 +0200

    update

--- Machine_learning/NLP/NLP_Code.py
+++ Machine_learning/NLP/NLP_Code.py
@@ -56,3 +56,36 @@ def text_process(mess): #Erstellung einer Funktion mit den oberen Variablen,dami
 
 test_for_tokenize =messages['message'].head().apply(text_process)
 
+##Vektorisierung
+
+#Aktuell liegen uns die Nachrichten als eine Listen von Tokens (auch als "Lemmas" bekannt) vor. Wir müssen jetzt jede dieser Nachrichten in einen Vektor umwandeln, mit dem SciKit Learn's Algorithmus arbeiten kann.
+
+#Wir werden dazu die folgenden drei Schritte nach dem bag-of-words (bow) Modell durchführen:
+
+#Zähle wie häufig ein Wort in jeder Nachricht vorkommt (Term Frequency)
+#Gewichte die Anzahl, so dass häufige Wörter ein niedrigeres Gewicht erhalten (Inverse Document Frequency)
+#Normalisiere den Vektor in Einheitslänge, um von der Länge des Originaltexts zu abstrahieren
+
+#Da es so viele Nachrichten gibt können wir häufig die Anzahl 0 erwarten.
+# Deshalb wird SciKit Learn eine Sparse Matrix ausgeben. siehe: https://de.wikipedia.org/wiki/D%C3%BCnnbesetzte_Matrix
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+BagOfWords_transformer = CountVectorizer(analyzer=text_process).fit(messages['message']) # Es wird die Methode text_process zum cleanen des Datasets genommen
+number_words= len(BagOfWords_transformer.vocabulary_) #wichtig ist das _ bei vocabulary_ damit das ganze Wörterverzeichnis genommen wird
+
+#Plausicheck für transformer
+
+message_test_4= messages['message'][3]
+
+BagOfWords_test_4=BagOfWords_transformer.transform([message_test_4])
+print(BagOfWords_test_4)
+print(BagOfWords_transformer.get_feature_names()[4068])
+
+# Es zeigt dass die Nummer 4068 das "U" ist und 2 Mal vorkommt - check plausibel
+
+BagOfWords_messages = BagOfWords_transformer.transform(messages['message'])
+
+sparsity = (100.0 * BagOfWords_messages.nnz / (BagOfWords_messages.shape[0] * BagOfWords_messages.shape[1]))
+
+
