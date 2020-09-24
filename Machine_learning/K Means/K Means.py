d44717fce4595a691224bc58b1078779f3e492a2
ThePeziBear <jernej.peter@gmail.com>
2020-09-24 11:27:04 +0200

    K Means with Sklearn

---
+++ Machine_learning/K Means/K Means.py
@@ -0,0 +1,37 @@
+# Erzeugung von Daten
+
+from numpy import random, array
+
+# Erstelle zufällige Einkommen / Alter - Cluster für N personen in k Cluster
+def createClusteredData(N, k):
+    random.seed(10)
+    pointsPerCluster = N/k
+    X = []
+    for i in range (k):
+        incomeCentroid = random.uniform(20000.0, 200000.0)
+        ageCentroid = random.uniform(20.0, 70.0)
+        for j in range(int(pointsPerCluster)):
+            X.append([random.normal(incomeCentroid, 10000.0), random.normal(ageCentroid, 2.0)])
+    X = array(X)
+    return X
+
+data=createClusteredData(100,5)
+
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import scale
+from numpy import random, float
+
+
+model = KMeans(n_clusters=5)
+
+# Hier werden die Daten normalisiert. Das ist wichtig, damit die Zurodnung sauber funktioniert!
+model = model.fit(scale(data))
+
+# Ausgabe der Cluster
+print(model.labels_)
+
+# Anzeigen der Daten in einem Grafik
+plt.figure(figsize=(8, 6))
+plt.scatter(data[:,0], data[:,1], c=model.labels_.astype(float))
+plt.show()
