cbeab0e21de822a7d8368157e82c82c147f21a1f
ThePeziBear <jernej.peter@gmail.com>
2020-10-02 15:29:24 +0200

    SVM mit Gridsearch V2

--- Machine_learning/Support Vector Machine/SVM_Code_Project_2.py
+++ Machine_learning/Support Vector Machine/SVM_Code_Project_2.py
@@ -5,17 +5,21 @@ y = iris['species']
 
 ## Train Test Split
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=101)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)#, random_state=101)
 
 
 # Suport Vector Machine
 from sklearn.svm import SVC
-svc_model = SVC(C=0.5, kernel='linear') #Instanzieren des Algorithmus;
-# C gibt die Toleranz der missklassifizierten Werte an. Je kleiner desto größer die Toleranz aus. default-value ist 1
+svc_model = SVC(C=1, kernel='linear') #Instanzieren des Algorithmus;
+# C (Soft Margin) gibt die Toleranz der missklassifizierten Werte an. Je kleiner desto größer die Toleranz. default-value ist 1
+# Das C verhält sich wie ein Regulierungsparameter in dem SVM.
 # Kernel kann linear, poly, rbf, sigmoid - siehe weiter: https://scikit-learn.org/stable/modules/svm.html#svm-kernels
 
 # Gamma definiert, wie viel Einfluss ein einzelnes Trainingsbeispiel hat.
 # Je größer gamma ist, desto näher müssen andere Beispiele betroffen sein.
+# Gamma ist ein freier Paramater von den gaußischen Radialbasisfunktion. Ein kleines Gamma bedeutet eine große Varianz --> SVM keine weitgestreuten Einfluss hat.
+# Ein großes Gamma führt zu einer hohen Bias und zu einer niedrigen Varianz. Ein niedriges Gamma führt zu einem niedrigen Bias und hohen Varianz.
+# siehe weiteres: https://scikit-learn.org/stable/auto_examples/svm/plot_rbf_parameters.html
 
 
 svc_model.fit(X_train,y_train) #Training
@@ -35,4 +39,24 @@ print(classification_report(y_test,predictions))
 # Dabei steht das CV für "Cross Validation".
 # Und dies wiederum bedeutet, dass GridSearchCV ein Dictionary verwendet, das die Parameter beschreibt, die getestet werden sollen, und ein Modell, das es zu trainieren gilt
 
-# Der
+## Gridsearch von Sklearn
+
+# Auswahl von verschiedenen Parametern für C und Gamma & die Kernel Funktion
+param_grid = {'C': [0.1,1, 10, 100, 1000], 'gamma': [1,0.1,0.01,0.001,0.0001], 'kernel': ['rbf']}
+
+from sklearn.model_selection import GridSearchCV
+grid = GridSearchCV(SVC(),param_grid,refit=True,verbose=3)
+# Estimator = SVC() - Angabe für das Modell
+# param_grid - gibt die Prameter zum durchtesten der Parameter
+# Refit = True - Beim Suchen der besten Parameter soll der Wert immer auf True sein.
+# verbose= gibt die Anzahl der Zwischenschritte wider
+
+grid.fit(X_train,y_train)
+
+grid.best_params_
+
+grid.best_estimator_
+
+grid_predictions = grid.predict(X_test)
+print(confusion_matrix(y_test,grid_predictions))
+print(classification_report(y_test,grid_predictions))
