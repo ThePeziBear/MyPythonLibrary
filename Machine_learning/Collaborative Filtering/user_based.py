d488ce3e9daeb7a59797886cf7a1c20fd64bc65c
ThePeziBear <jernej.peter@gmail.com>
2020-10-10 21:16:43 +0200

    collaborative Filtering (user-based) - bug fixed

--- Machine_learning/Collaborative Filtering/user_based.py
+++ Machine_learning/Collaborative Filtering/user_based.py
@@ -44,10 +44,19 @@ similarMovies=similarMovies.dropna()
 similarMovies=pd.DataFrame(similarMovies)
 similarMovies=similarMovies.sort_values(by=0,ascending=False)
 
+#Wir erstellen daher ein neues DataFrame, welches zu jedem Film die Anzahl speichert, wie oft dieser Film bewertet wurde
+# und was die durchschnittliche Bewertung war. Das könnte später nützlich sein:
+df1=pd.DataFrame(ratings.groupby('title')['rating'].count())
+df2=pd.DataFrame(ratings.groupby('title')['rating'].mean())
+df_merged=pd.merge(df1,df2,how='inner',left_on='title',right_on='title')
+df_merged=df_merged.rename(columns={"rating_x": "Count", "rating_y": "Mean", 0:'Similarity'})
+
+
+
 #Jetzt entfernen wir alle Filme, die von weniger als 100 Personen bewertet wurden, und schauen uns die übrig gebliebenen an:
-df_100 = df_merged[df_merged['rating_x'] >100]
+df_100 = df_merged[df_merged['Count'] >100]
 df_100=pd.merge(df_100,similarMovies,how='inner',left_on='title',right_on='title')
-df_100=df_100.rename(columns={"rating_x": "Count", "rating_y": "Mean", 0:'Similarity'})
+df_100=df_100.rename(columns={0:'Similarity'})
 df_100=df_100.sort_values('Similarity', ascending = False)# - Sortierung der Werte auf(ascending=True) oder absteigend (ascending=False)
 df_100.head(12)
 # Jetzt macht die Modellierung Sinn!!
