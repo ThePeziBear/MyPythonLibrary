e7af06f295e1f680c656ffc042e80a4a22ce74e0
ThePeziBear <jernej.peter@gmail.com>
2020-11-07 17:19:45 +0100

    Data Exploration

--- Masterthesis/Red_Wine_Quality/Data_Analysis_Winequality_red.py
+++ Masterthesis/Red_Wine_Quality/Data_Analysis_Winequality_red.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
 
 df=pd.read_csv('winequality-red.csv',sep=',')
 
@@ -9,8 +11,18 @@ df_feat=df.drop(columns=['quality'])
 print(df_feat)
 
 df_target=df['quality']
+df_top_wine=df[df['quality']>=5]
+#sns.pairplot(df,hue='quality',palette='coolwarm')
 
-sns.pairplot(df,hue='quality',palette='coolwarm')
 
-plt.scatter(df['citric acid'],df['fixed acidity'])
-sns.stripplot(x='volatile acidity',y='fixed acidity', data=df,hue='quality',palette='Set1')
+X=df_feat
+y=df_target
+
+X1=sm.add_constant(X)
+est=sm.OLS(y,X1).fit()
+summary_stat=est.summary()
+
+#plt.scatter(df['citric acid'],df['fixed acidity'])
+sns.stripplot(x='volatile acidity',y='fixed acidity', data=df_top_wine,hue='quality',palette='Set1')
+
+
