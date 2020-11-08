d725386daec734dbd566393464c19af5fd310f20
ThePeziBear <jernej.peter@gmail.com>
2020-11-08 12:59:09 +0100

    Data Exploration

--- Masterthesis/Red_Wine_Quality/Data_Analysis_Winequality_red.py
+++ Masterthesis/Red_Wine_Quality/Data_Analysis_Winequality_red.py
@@ -22,7 +22,17 @@ X1=sm.add_constant(X)
 est=sm.OLS(y,X1).fit()
 summary_stat=est.summary()
 
-#plt.scatter(df['citric acid'],df['fixed acidity'])
-#sns.stripplot(x='volatile acidity',y='fixed acidity', data=df_top_wine,hue='quality',palette='Set1')
+#plt.scatter(X,y)
+#plt.scatter(df['volatile acidity'],df['fixed acidity'])
 
+X_array=X.to_numpy()
 
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2, whiten=True).fit(X)
+X_pca = pca.transform(X)
+varianz_pca= pca.explained_variance_ratio_ #Erklärt wieviel Varianz aufgrund der Reduktion erhalten wurde
+print(X_pca.shape) #Zeigt dass die Daten auf 6Dimensionen reduziert wurden.
+
+df_com=pd.DataFrame(X_pca,columns=['f1','f2'])
+df_com['quality']=y
+sns.stripplot(x='f1',y='f2',hue='quality',data=df_com)
