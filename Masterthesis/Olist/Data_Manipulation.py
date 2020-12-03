04b8f45a294bf3be83849ca8ad20b7624ef6597e
ThePeziBear <jernej.peter@gmail.com>
2020-12-03 18:16:18 +0100

    Olist Dataset - Data-Exploration

--- Masterthesis/Olist/Data_Manipulation.py
+++ Masterthesis/Olist/Data_Manipulation.py
@@ -23,21 +23,13 @@ df_count_items=df_count_items.sort_values(by='order_id', ascending=False)
 df_count_items=(df_count_items.iloc[:5000000]).sort_values(by='order_id', ascending=False)
 df_count_items
 
-barblot_product1 = df_count_items['order_id'].plot.bar(stacked=True)
+#barblot_product1 = df_count_items['order_id'].plot.bar(stacked=True)
 
 #Merge dataframes by 'product_id','product_category_name','order_item_id','price','seller_id','freight_value','product_description_lenght','product_photos_qty','review_score','review_comment_title','review_comment_message'
 dfmerge = pd.merge(df_order_items,df_products, how='inner', left_on='product_id', right_on='product_id')#.set_index('Auswahl Spalte als Index')
 
-dfmerge_2=pd.merge(dfmerge,df_order_reviews,on='product_id')
-
-
-dfmerge2=pd.merge(dfmerge,df_order_reviews,how='left',left_on='order_id',right_on='order_id')
-
-
-dfmerge_count=dfmerge.set_index(["order_id", "seller_id"]).count(level="order_id")
-dfmerge_count_2=dfmerge_count.sort_values('product_id',ascending=False)
-
-df_test=dfmerge[dfmerge['order_id']=='8272b63d03f5f79c56e9e4120aec44ef']
+dfmerge2=pd.merge(dfmerge,df_order_reviews,on='order_id')
+dfmerge2_count=dfmerge2.groupby('review_score').count()
 
 
 dfmerge3=dfmerge2[['product_id','product_category_name','order_item_id','price','seller_id','freight_value','product_description_lenght','product_photos_qty','review_score','review_comment_title','review_comment_message']]
@@ -53,6 +45,23 @@ df_cool_stuff=df_merge2[df_merge2['product_category_name']=='cool_stuff']
 df_count_reviews=df_order_reviews.groupby('review_score').count()
 barplot_review1 = df_count_reviews['review_id'].plot.bar(stacked=True)
 #Features:product_id,
+
+#Datavisualierung dfset_1
+
+subset1=dfset_1[['product_description_lenght','product_photos_qty','review_score']]
+
+sns.lmplot(x='product_description_lenght',y='product_photos_qty',data=dfset_1,hue='review_score',palette='coolwarm'),#hue: Trennung von kategorischen Parameter
+            #markers=['o','v'], #Veränderung der Symbole
+            #scatter_kws={'s':100}) #scatter_kws: Definition von der Göße der Symbole
+plt.show()
+
+
+X=dfset_1[['product_description_lenght','product_photos_qty']]
+y=dfset_1['review_score']
+
+
+
+
 
 #2 Predict Sales aufgrund von Produktfotos Und/Oder Länge Produktnamen
 
